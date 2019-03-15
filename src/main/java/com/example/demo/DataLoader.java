5c9e53491f555f12806eeec6b95fe84139a9704e
mhussainshah1 <muhammadshah.14@sunymaritime.edu>
2019-03-15 00:28:49 -0400

    remove css

--- src/main/java/com/example/demo/DataLoader.java
+++ src/main/java/com/example/demo/DataLoader.java
@@ -12,13 +12,13 @@ public class DataLoader implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        Task task = new Task("grocery", "chicken, eggs, bread", "https://res.cloudinary.com/mhussainshah1/image/upload/v1551473204/java-bootcamp/high-priority.jpg", true);
+        Task task = new Task("grocery", "chicken, eggs, bread", "https://res.cloudinary.com/mhussainshah1/image/upload/c_fill,g_face,h_30,r_50,w_30/v1551473204/java-bootcamp/high-priority.jpg", true);
         repository.save(task);
 
-        task = new Task("laundry", "white and color clothes", "https://res.cloudinary.com/mhussainshah1/image/upload/v1551473204/java-bootcamp/medium-priority.png", false);
+        task = new Task("laundry", "white and color clothes", "https://res.cloudinary.com/mhussainshah1/image/upload/c_fill,g_face,h_30,r_50,w_30/v1551473204/java-bootcamp/medium-priority.png", false);
         repository.save(task);
 
-        task = new Task("shopping", "glasses, napkins and spices", "https://res.cloudinary.com/mhussainshah1/image/upload/v1551473204/java-bootcamp/low-priority.jpg", true);
+        task = new Task("shopping", "glasses, napkins and spices", "https://res.cloudinary.com/mhussainshah1/image/upload/c_fill,g_face,h_30,r_50,w_30/v1551473204/java-bootcamp/low-priority.jpg", true);
         repository.save(task);
     }
 }
